919a97003384adc50b71d17d6d29b8eea4766b62
lovezhangchuangxin <2911331070@qq.com>
2025-02-14 16:48:44 +0800

    feat: 优化minPathSum

--- 64.minimum-path-sum/test.cpp
+++ 64.minimum-path-sum/test.cpp
@@ -8,4 +8,7 @@ TEST_CASE("testing minPathSum")
 
     vector<vector<int>> grid1 = {{1, 2, 3}, {4, 5, 6}};
     CHECK(solution.minPathSum(grid1) == 12);
+
+    vector<vector<int>> grid2 = {{1, 2}, {1, 1}};
+    CHECK(solution.minPathSum(grid2) == 3);
 }
